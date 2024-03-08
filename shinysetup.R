57354444b5a2f0fe7de6c87c901efc9ab2444f1f
Kevin Middleton <middletonk@missouri.edu>
2024-03-07 20:35:41 -0600

    Add shinylive test site

---
+++ shinysetup.R
@@ -0,0 +1,7 @@
+library(shinylive)
+
+# Copy "Hello World" from `{shiny}`
+system.file("examples", "01_hello", package="shiny") |>
+  fs::dir_copy("myapp", overwrite = TRUE)
+
+shinylive::export("myapp", "site")
